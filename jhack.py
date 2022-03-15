232b763ea2809f3f84ad54ba619866ffab131578
Pietro Pasotti <starfire.daemon@gmail.com>
2022-03-15 14:39:29 +0100

    added unfuck_juju script

--- jhack.py
+++ jhack.py
@@ -1,13 +1,14 @@
 #!/bin/python3
 import asyncio
-import os
-import time
 import contextlib
 import logging
-from typing import List, Set
-from subprocess import Popen, PIPE
-
+import os
+import time
 from pathlib import Path
+from subprocess import Popen, PIPE
+from typing import List
+
+import typer
 from juju import jasyncio
 from juju.application import Application
 from juju.model import Model
@@ -86,7 +87,6 @@ async def push(file: Path, remote_root: str, app, unit, container_name):
     container_opt = f"--container {container_name} " if container_name else ""
     cmd = f"juju scp {container_opt}{file} {app}/{unit}:{remote_file_path}"
     proc = Popen(cmd.split(' '), stdout=PIPE, stderr=PIPE)
-
     retcode = proc.returncode
     if retcode != None:
         logger.error(f"{cmd} errored with code {retcode}: "
@@ -94,6 +94,77 @@ async def push(file: Path, remote_root: str, app, unit, container_name):
                      f"\nstderr={proc.stderr.read()}")
 
     logger.info(f'synced {file}')
+
+
+async def _remove_model(model_name: str, force=True,
+                        no_wait=True, destroy_storage=True,
+                        dry_run=False):
+    cmd = f'juju destroy-model {model_name} ' \
+          f'{"--force " if force else ""}' \
+          f'{"--no-wait " if no_wait else ""}' \
+          f'{"--destroy-storage " if destroy_storage else ""}-y'
+
+    if dry_run:
+        logger.info(f'would destroy model {model_name} with: {cmd!r}')
+        return
+    else:
+        logger.info(f'destroying model {model_name} ({cmd})')
+
+    proc = Popen(cmd.split(' '), stdout=PIPE, stderr=PIPE)
+    logger.info(f'spawned off model destroyer to pid={proc.pid}')
+    proc.wait()
+    if proc.returncode != 0:
+        raise RuntimeError(
+            f"{cmd!r} failed with retcode {proc.returncode!r}; "
+            f"\nstdout={proc.stdout.read().decode('utf-8')}"
+            f"\nstderr={proc.stdout.read().decode('utf-8')}"
+        )
+
+
+def rmodel(
+        model_name=typer.Argument(
+            'model_name',
+            help='comma-separated list of models to be removed, or single '
+                 'globbed name'),
+        force: bool = True,
+        no_wait: bool = True,
+        destroy_storage: bool = True,
+        dry_run: bool = False):
+    if '*' in model_name:
+        if model_name.endswith('*'):
+            method = str.startswith
+        elif model_name.startswith('*'):
+            method = str.endswith
+        else:
+            raise ValueError(
+                f'invalid globbing: {model_name!r}; * only supported '
+                f'at the end or start of a pattern'
+            )
+        filterer = lambda name: method(name, model_name.strip('*'))
+
+        proc = Popen('juju models'.split(' '), stdout=PIPE)
+        raw = proc.stdout.read().decode('utf-8').strip()
+        lines = raw.split('\n')[4:]
+        all_models = [line.split(' ')[0].strip('*') for line in lines]
+        to_remove = tuple(filter(filterer, all_models))
+        if not to_remove:
+            logger.info(
+                f'Globbed name {model_name!r} yielded no matches; method {all_models}.')
+
+    else:
+        to_remove = model_name.split(',')
+
+    if not to_remove:
+        logger.info('Nothing to remove.')
+        return
+
+    _remove_fn = lambda model: _remove_model(
+        model, force, no_wait, destroy_storage, dry_run)
+    logger.info('Preparing to remove\n\t' + '\n\t'.join(to_remove))
+    asyncio.get_event_loop().run_until_complete(
+        jasyncio.gather(*(_remove_fn(model) for model in to_remove))
+    )
+    logger.info('Done.')
 
 
 def sync(local_folder: str,
@@ -160,9 +231,39 @@ def sync(local_folder: str,
         time.sleep(polling_interval)
 
 
+def unfuck_juju(model_name: str = 'foo',
+                controller_name: str = 'mk8scloud',
+                juju_channel: str = 'stable',
+                microk8s_channel: str = 'stable'):
+    unfuck_juju_script = Path(__file__).parent / 'unfuck_juju'
+    if not os.access(unfuck_juju_script, os.X_OK):
+        raise RuntimeError(
+            'unfuck_juju script is not executable. Ensure it has X permissions.'
+        )
+    if not unfuck_juju_script.exists():
+        raise RuntimeError(
+            f'unable to locate unfuck_juju shell script '
+            f'({unfuck_juju_script!r})'
+        )
+
+    cmd = [str(unfuck_juju_script),
+           '-J', juju_channel,
+           '-M', microk8s_channel,
+           '-m', model_name,
+           '-c', controller_name]
+
+    proc = Popen(cmd)
+    proc.wait()
+    if returncode := proc.returncode != 0:
+        logger.error(f"{cmd} failed with return code {returncode}")
+        logger.error(proc.stdout.read().decode('utf-8'))
+        logger.error(proc.stderr.read().decode('utf-8'))
+    else:
+        print(proc.stdout.read().decode('utf-8'))
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    import typer
 
     model = typer.Typer(name='model')
 
@@ -175,9 +276,14 @@ if __name__ == '__main__':
     utils = typer.Typer(name='utils')
 
     sync_cmd = utils.command()(sync)
+    unfuck_juju_cmd = utils.command()(unfuck_juju)
+
+    ctr = typer.Typer(name='ctr')
+    rmodel = ctr.command()(rmodel)
 
     app = typer.Typer(name='jhack')
     app.add_typer(model)
     app.add_typer(utils)
+    app.add_typer(ctr)
 
     app()
