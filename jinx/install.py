905795936eb9a7ad19dc08f07746d0355f2f869d
Pietro Pasotti <starfire.daemon@gmail.com>
2022-06-02 14:23:10 +0200

    fixed install script

--- jinx/install.py
+++ jinx/install.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from subprocess import Popen
 from time import sleep
 
-path = Path(__file__)
-path_to_jinx = path.parent / 'jinx'
+jinx_root = Path(__file__).parent
+path_to_jinx = jinx_root / 'jinx'
 
 
 def jinx_installed() -> bool:
@@ -12,8 +12,8 @@ def jinx_installed() -> bool:
 
 def install():
     """Install jinx source and unpack script."""
-    script = "cd ./jinx; git clone https://github.com/PietroPasotti/jinx"
-    proc = Popen(script.split(' '))
+    script = "git clone https://github.com/PietroPasotti/jinx"
+    proc = Popen(script.split(' '), cwd=jinx_root)
     proc.wait()
     while proc.returncode is None:
         sleep(.1)
