e026ec42f7417ec63e57e6d0e195bb7da3201962
Pietro Pasotti <starfire.daemon@gmail.com>
2022-06-02 14:21:04 +0200

    added jinx installed guard to pack

--- jinx/pack.py
+++ jinx/pack.py
@@ -3,9 +3,15 @@ from datetime import datetime
 from subprocess import Popen
 from time import sleep
 
+from jinx.install import jinx_installed
+
 
 def pack_jinx():
     """Packs a jinx. Alias for jinx.unpack; charmcraft pack."""
+    if not jinx_installed():
+        print('run jhack jinx install first.')
+        return
+
     begin = datetime.now()
     cmd = Popen('unpack; charmcraft pack'.split())
     cmd.wait()
