9659ae823f9b21cc8a31cdfe35964443c23b8bc0
Pietro Pasotti <starfire.daemon@gmail.com>
2022-06-14 12:10:27 +0200

    fixed issue with pack

--- jinx/pack.py
+++ jinx/pack.py
@@ -22,21 +22,36 @@ def pack(charm_source: Path = Argument(
 
     if not charm_source:
         print('charm_source not provided, guessing `./src/charm.py`')
-    charm_source = Path(
-        charm_source) if charm_source else Path() / 'src' / 'charm.py'
+    charm_source = (Path(charm_source) if charm_source else
+                    Path() / 'src' / 'charm.py').absolute()
+
     if not charm_source.name == 'charm.py':
         print(f'expected charm.py file, not {charm_source}')
         return
     begin = datetime.now()
-    env = os.environ
+
+    charm_root = charm_source.parent.parent
+
+    # unpack jinx to source
     cmd = Popen(
         f'{PATH_TO_JINX_PACKAGE}/unpack.py --overwrite ./src/charm.py'.split(),
-        cwd=charm_source.parent.parent)
+        cwd=charm_root)
     cmd.wait()
 
+    # copy over jinx source as lib
+    libs = charm_root / 'lib' / 'charms'
+    if not libs.exists():
+        os.makedirs(libs)
+
+    cmd = Popen(
+        f"cp {PATH_TO_JINX_PACKAGE / 'jinx.py'} {libs / 'jinx.py'}".split(),
+        cwd=charm_root)
+    cmd.wait()
+
+    # charmcraft pack
     cmd = Popen(
         'charmcraft pack'.split(),
-        cwd=charm_source.parent.parent)
+        cwd=charm_root)
     cmd.wait()
 
     while cmd.returncode == None:
