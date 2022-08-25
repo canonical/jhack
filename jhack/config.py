693aa030e4c6c6e336fa4472701dc5c187cd4d93
Pietro Pasotti <starfire.daemon@gmail.com>
2022-08-25 16:14:22 +0200

    fixed some strict confinement issues:

--- jhack/config.py
+++ jhack/config.py
@@ -1,10 +1,7 @@
 import os
-from pathlib import Path
 from subprocess import CalledProcessError, check_output
 
 from jhack.logger import logger
-
-default_juju_data_loc = "~/.local/share/juju/"
 
 
 def configure():
@@ -25,38 +22,8 @@ def configure():
             "juju command not found. "
             "All jhacks depending on juju calls will bork."
             "if this is a snap, you might have forgotten to "
-            "connect jhack to juju."
+            "connect jhack to some required plugs."
         )
-
-    # check that `JUJU_DATA` is set. if not, default it.
-    jdata_key = "JUJU_DATA"
-    jdata = os.environ.get(jdata_key)
-    logger.warning(f"JUJU_DATA is {jdata}")
-
-    config_file = Path(snap_data) / "config"
-    if not config_file.exists():
-        logger.warning(f"no config file found at: {config_file}.")
-        logger.warning(f"setting JUJU_DATA to default: {default_juju_data_loc}")
-        juju_data_loc = default_juju_data_loc
-    else:
-        logger.debug(f"config file found at: {config_file}.")
-        try:
-            juju_data_loc = config_file.read_text().split("=")[1].strip()
-        except (IndexError, TypeError) as e:
-            logger.error(
-                f"error parsing config file: {e}."
-                f"Will fall back to default juju data "
-                f"location: {default_juju_data_loc}"
-            )
-            juju_data_loc = default_juju_data_loc
-        if not juju_data_loc or not Path(juju_data_loc).exists():
-            logger.warning(
-                f"invalid juju data path: {juju_data_loc}. "
-                f"Jhack will likely misbehave"
-            )
-
-    logger.warning(f"setting JUJU_DATA to: {juju_data_loc}")
-    os.environ[jdata_key] = juju_data_loc
 
 
 if __name__ == "__main__":
