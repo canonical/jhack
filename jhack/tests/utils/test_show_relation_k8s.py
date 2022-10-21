96f02e3e4fcf04947c146ab8dc00f3a2b9de26c1
Pietro Pasotti <starfire.daemon@gmail.com>
2022-10-21 10:18:03 +0200

    fixed show-relation bug

--- jhack/tests/utils/test_show_relation_k8s.py
+++ jhack/tests/utils/test_show_relation_k8s.py
@@ -1,13 +1,14 @@
 import json as json_
 import sys
 from pathlib import Path
+from textwrap import dedent
 from unittest.mock import patch
 
 sys.path.append(str(Path(__file__).parent.parent.parent))
 
 import pytest
 
-from jhack.utils.show_relation import _sync_show_relation, sync_show_relation
+from jhack.utils.show_relation import _sync_show_relation, get_interface
 
 
 def fake_juju_status(app_name, model=None, json: bool = False):
@@ -67,3 +68,56 @@ def mock_stdout():
 )
 def test_show_unit_works(ep1, ep2, n):
     _sync_show_relation(endpoint1=ep1, endpoint2=ep2, n=n)
+
+
+@pytest.mark.parametrize(
+    "app_name, relation_name, other_app_name, other_relation_name, expected_interface_name",
+    (
+        ("postgresql-k8s", "database", "kratos", "pg-database", "postgresql_client"),
+        (
+            "postgresql-k8s",
+            "database-peers",
+            "postgresql-k8s",
+            "database-peers",
+            "postgresql_peers",
+        ),
+        ("postgresql-k8s", "restart", "postgresql-k8s", "restart", "rolling_op"),
+    ),
+)
+def test_intf_re(
+    app_name,
+    relation_name,
+    other_app_name,
+    other_relation_name,
+    expected_interface_name,
+):
+    status = dedent(
+        """
+    Model   Controller  Cloud/Region        Version  SLA          Timestamp
+    kratos  micro       microk8s/localhost  2.9.34   unsupported  14:43:53-04:00
+    
+    
+    App             Version  Status  Scale  Charm           Channel  Rev  Address         Exposed  Message
+    kratos                   active      1  kratos                     2  10.152.183.124  no       
+    postgresql-k8s           active      1  postgresql-k8s  edge      25  10.152.183.219  no       Primary
+    
+    Unit               Workload  Agent  Address     Ports              Message
+    kratos/0*          active    idle   10.1.64.94  4434/TCP,4433/TCP  
+    postgresql-k8s/0*  active    idle   10.1.64.89                     Primary
+    
+    Relation provider              Requirer                       Interface          Type     Message
+    postgresql-k8s:database        kratos:pg-database             postgresql_client  regular  
+    postgresql-k8s:database-peers  postgresql-k8s:database-peers  postgresql_peers   peer     
+    postgresql-k8s:restart         postgresql-k8s:restart         rolling_op         peer
+    """
+    )
+
+    interface = get_interface(
+        status,
+        app_name=app_name,
+        relation_name=relation_name,
+        other_app_name=other_app_name,
+        other_relation_name=other_relation_name,
+    )
+
+    assert interface == expected_interface_name
