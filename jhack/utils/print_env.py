51e6818daec7cd22fe8b0c44625373fccaf73cde
Pietro Pasotti <starfire.daemon@gmail.com>
2023-04-19 09:37:54 +0200

    niceified printenv

--- jhack/utils/print_env.py
+++ jhack/utils/print_env.py
@@ -1,13 +1,26 @@
 import csv
 import subprocess
-import textwrap
+import toml
+from json import dumps as json_dumps, loads as json_loads
+from pathlib import Path
+from typing import Optional
 
-NO_VERSION = "Not Installed."
+import typer
+from rich.console import Console
+from rich.table import Table
+from jhack.logger import logger as jhack_logger
+
+NOT_INSTALLED = "Not Installed."
+logger = jhack_logger.getChild(__name__)
 
 
-def get_output(command: str):
-    p = subprocess.run(command.split(), capture_output=True, text=True)
-    return p.stdout.strip() if p.returncode != 127 else None
+def get_output(command: str) -> Optional[str]:
+    try:
+        p = subprocess.run(command.split(), capture_output=True, text=True)
+        return p.stdout.strip() if p.returncode == 0 else None
+    except subprocess.CalledProcessError as e:
+        logger.info(e)
+        return None
 
 
 def get_os_release():
@@ -15,25 +28,43 @@ def get_os_release():
         return dict(csv.reader(f, delimiter="="))
 
 
-def print_env():
+def print_env(json: bool = typer.Option(False, is_flag=True)):
     """Print the details of the juju environment for use in bug reports."""
-    juju_version = get_output("juju --version") or NO_VERSION
-    mk8s_version = get_output("microk8s version") or NO_VERSION
-    lxd_version = get_output("lxd --version") or NO_VERSION
+    pyproject_toml = Path(__file__).parent.parent.parent.absolute().joinpath('pyproject.toml')
+    jhack_version = toml.loads(pyproject_toml.read_text())['project']['version']
+
+    juju_version = get_output("juju --version")
+    mk8s_version = get_output("microk8s version")
+    lxd_version = get_output("lxd --version")
+    multipass_version = get_output("multipass version --format json")
+    if multipass_version:
+        multipass_version = json_loads(multipass_version)
+    else:
+        multipass_version = {}
     os_version = get_os_release()["PRETTY_NAME"]
     kernel_info = get_output("uname -srp")
 
-    print(
-        textwrap.dedent(
-            f"""\
-            juju:
-                {juju_version}
-            microk8s:
-                {mk8s_version}
-            lxd:
-                {lxd_version}
-            os-info:
-                {os_version}
-                {kernel_info}"""
-        )
-    )
+    data = {
+        'jhack': jhack_version,
+        'juju': juju_version,
+        'microk8s': mk8s_version,
+        'lxd': lxd_version,
+        'multipass': multipass_version.get('multipass', None),
+        'multipassd': multipass_version.get('multipassd', None),
+        'os': os_version,
+        'kernel': kernel_info,
+    }
+
+    if json:
+        jsn = json_dumps(data, indent=2)
+        print(jsn)
+
+    else:
+        table = Table(title="juju info v0.1", show_header=False)
+        for k, v in data.items():
+            table.add_row(k, v or NOT_INSTALLED)
+        Console().print(table)
+
+
+if __name__ == '__main__':
+    print_env(False)
