6d3c36722c02d86f39d002a471c06877c7f9a794
Pietro Pasotti <starfire.daemon@gmail.com>
2023-10-06 08:39:28 +0200

    k8s fire with sudo

--- jhack/utils/simulate_event.py
+++ jhack/utils/simulate_event.py
@@ -165,8 +165,7 @@ def _simulate_event(
         raise RuntimeError("is juju installed?")
 
     juju_exec_cmd = "/usr/bin/" + ("juju-exec" if version >= (3, 0) else "juju-run")
-    if get_substrate(_model) != "k8s":
-        juju_exec_cmd = "sudo " + juju_exec_cmd
+    juju_exec_cmd = "sudo " + juju_exec_cmd
 
     cmd = f"juju ssh {_model}{unit} {juju_exec_cmd} -u {unit} {env} ./dispatch"
 
