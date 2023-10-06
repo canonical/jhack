0c656ebed384209db9d4bba6837ecf4802a2b070
Pietro Pasotti <starfire.daemon@gmail.com>
2023-10-06 10:01:37 +0200

    vinfo for local charms

--- jhack/charm/vinfo.py
+++ jhack/charm/vinfo.py
@@ -2,9 +2,12 @@ import json
 from dataclasses import dataclass
 from enum import Enum
 from operator import itemgetter
-from typing import Optional, Tuple
+from pathlib import Path
+from subprocess import getoutput, getstatusoutput
+from typing import Optional, Tuple, Union
 
 import typer
+import yaml
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
@@ -16,6 +19,7 @@ from jhack.helpers import (
     cached_juju_status,
     check_command_available,
     get_libinfo,
+    get_local_libinfo,
 )
 from jhack.logger import logger as jhack_logger
 
@@ -32,6 +36,27 @@ def _check_outdated(charm_name):
     # so we get to try first with dashes.
     dashed_name = charm_name.replace("_", "-")
     return _get_charmcraft_lib_info(dashed_name) or _get_charmcraft_lib_info(charm_name)
+
+
+def _add_local_charm_info(table: Table, target: Path):
+    name = yaml.safe_load((target / "metadata.yaml").read_text())["name"]
+    table.add_row(
+        "local charm name",
+        name,
+    )
+    code, out = getstatusoutput("git rev-parse --abbrev-ref HEAD")
+    if code == 0:
+        table.add_row(
+            "branch",
+            out,
+            end_section=True,
+        )
+    else:
+        table.add_row(
+            "branch",
+            "<unable to parse ref HEAD>",
+            end_section=True,
+        )
 
 
 def _add_app_info(table: Table, target: str, model: str):
@@ -73,7 +98,12 @@ class OutdatedCheck:
 
 
 def _add_charm_lib_info(
-    table: Table, app: str, model: str, check_outdated=True, machine=False
+    table: Table,
+    target: Union[str, Path],
+    model: Optional[str] = None,
+    check_outdated=True,
+    machine=False,
+    is_path=False,
 ):
     if check_outdated and not check_command_available("charmcraft"):
         logger.error(
@@ -82,7 +112,10 @@ def _add_charm_lib_info(
         )
         check_outdated = False
 
-    libinfo = get_libinfo(app, model, machine)
+    if is_path:
+        libinfo = get_local_libinfo(target)
+    else:
+        libinfo = get_libinfo(target, model, machine)
 
     ch_lib_meta = {}
 
@@ -162,24 +195,59 @@ def _add_charm_lib_info(
     table.rows[-1].end_section = True
 
 
+def _is_path(s: str) -> bool:
+    """Determine if s refers to a juju unit or a path."""
+    if s.startswith(".") or s.startswith("/"):
+        return True
+    return False
+
+
+class NotACharmError(RuntimeError):
+    pass
+
+
+def _check_local_charm(target: Path):
+    if (
+        not target.is_dir()
+        or not (target / "metadata.yaml").exists()
+        or not (target / "src" / "charm.py").exists()
+    ):
+        raise NotACharmError(f"{target} does not appear to be a charm root.")
+
+
 def _vinfo(
     target: str,
+    is_path: Optional[bool] = None,
     machine: bool = False,
     check_outdated: bool = True,
     color: RichSupportedColorOptions = "auto",
-    model: str = None,
+    model: Optional[str] = None,
 ):
-    table = Table(title="vinfo v0.1", show_header=False, expand=True)
+    table = Table(title="vinfo v0.2", show_header=False, expand=True)
     table.add_column()
     table.add_column()
 
-    _add_app_info(table, target, model)
+    if is_path is None:
+        is_path = _is_path(target)
+    if is_path and model:
+        logger.warning(
+            f"ignoring `model={model}` since the target appears to be a path."
+        )
+
+    if is_path:
+        target = Path(target)
+        _check_local_charm(target)
+        _add_local_charm_info(table, target)
+    else:
+        _add_app_info(table, target, model)
+
     _add_charm_lib_info(
         table,
         target,
         model,
         machine=machine,
         check_outdated=check_outdated,
+        is_path=is_path,
     )
 
     if color == "no":
@@ -190,7 +258,19 @@ def _vinfo(
 
 def vinfo(
     target: str = typer.Argument(
-        ..., help="Unit or application name to generate the vinfo of."
+        ...,
+        help="Unit or application name to generate the vinfo of. "
+        "If you pass -p|--path, this will be interpreted as a charm repository root dir. "
+        "If it's obviously a unit name or obviously a path (starts with `./` or `/`), "
+        "jhack will do the smart thing, but if there's "
+        "ambiguity it will default to unit.",
+    ),
+    is_path: Optional[bool] = typer.Option(
+        None,
+        "-p",
+        "--path",
+        is_flag=True,
+        help="Whether 'target' should be interpreted as a path to a local charm.",
     ),
     check_outdated: bool = typer.Option(
         False,
@@ -209,6 +289,7 @@ def vinfo(
     """Show version information of a charm and its charm libs."""
     _vinfo(
         target=target,
+        is_path=is_path,
         machine=False,  # not implemented; todo implement
         check_outdated=check_outdated,
         color=color,
