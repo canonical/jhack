c757677dadac38b9700d57f6b0ba08d7f0869109
Phan Trung Thanh <trung.thanh.phan@canonical.com>
2024-08-30 10:16:32 +0200

    Update docs for --source option

--- jhack/utils/sync.py
+++ jhack/utils/sync.py
@@ -270,7 +270,7 @@ def sync(
         "--source",
         "-s",
         help="Local directories to watch for changes. "
-        "Semicolon-separated list of directories.",
+        "Repeat for each directory (e.g --source ./src --source .lib).",
     ),
     remote_root: str = typer.Option(
         None,
