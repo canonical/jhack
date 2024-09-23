cd041e00cf43d9cecbb74dbccdb39a54dd9c7263
Pietro Pasotti <starfire.daemon@gmail.com>
2024-09-23 12:41:45 +0200

    qfd

--- jhack/utils/tail_logs.py
+++ jhack/utils/tail_logs.py
@@ -1,20 +1,19 @@
-import os
 import shlex
-import select
+import shlex
 import subprocess
 from dataclasses import dataclass
-from multiprocessing.pool import ThreadPool
+from datetime import datetime
 from pathlib import Path
-from platform import system
 from tempfile import NamedTemporaryFile
 from time import sleep
-from typing import Tuple
+from typing import Tuple, Iterable, Dict
 
 import yaml
-from rich.console import Console
-from rich.layout import Layout
+from rich.console import ConsoleOptions, Console
+from rich.layout import Layout, RenderMap
 from rich.live import Live
 from rich.panel import Panel
+from rich.table import Table
 
 from jhack.helpers import Target, fetch_file
 from jhack.logger import logger as jhack_logger
@@ -22,63 +21,69 @@ from jhack.logger import logger as jhack_logger
 logger = jhack_logger.getChild("tail_logs")
 
 
-class ConsolePane:
-    def __init__(self):
-        self.content = ""
+class Pane(Table):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
-    def print(self, msg: str):
-        self.content += msg
+        self.trimmed = []
 
-    def __rich_console__(self, console, options):
-        for line in console.render_lines(
-            self.content, options=options, pad=False, new_lines=False
-        )[-options.height :]:
-            yield from line
+    def trim(self, size: int):
+        if self.row_count > size:
+            self.trimmed.extend(self.rows[:-size])
+            self.rows = self.rows[-size:]
+        else:
+            while self.row_count < size:
+                self.add_row(self.trimmed.pop(0))
 
-
-def _redirect_logs(command, pane: ConsolePane):
-    logger.debug(f"starting log redirection with {command}")
-    proc = subprocess.Popen(
-        command,
-        universal_newlines=True,
-        bufsize=1000,
-        stdout=subprocess.PIPE,
-    )
-    poll = select.poll()
-    poll.register(proc.stdout)
-
-    while True:
-        if poll.poll(1):
-            for line in proc.stdout.readlines():
-                pane.print(line.strip())
+    def clear(self):
+        self.trimmed.extend(self.rows)
+        self.rows = []
 
 
-def _tail_juju_debug_log(target: Target, pane: ConsolePane):
-    cmd = f"juju debug-log --replay -i {target.unit_name}"
-    _redirect_logs(shlex.split(cmd), pane)
+class PaneLayout(Layout):
+    def __init__(self, pane: Pane, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.pane = pane
 
-
-def _pebble_cmd(target: Target, *, command: str, container: str = "charm"):
-    return shlex.split(
-        f"juju ssh {target.unit_name} PEBBLE_SOCKET=/charm/containers/{container}/pebble.socket /charm/bin/pebble {command}"
-    )
+    def render(self, console: Console, options: ConsoleOptions) -> RenderMap:
+        rm = super().render(console, options)
+        max_table_len = rm[self].region.height
+        self.pane.trim(max_table_len)
+        return rm
 
 
 def _pebble(target: Target, *, command: str, container: str = "charm"):
     """Run a pebble command on a unit."""
-    cmd = _pebble_cmd(target, command=command, container=container)
-    proc = subprocess.Popen(
-        cmd, universal_newlines=True, bufsize=1000, stdout=subprocess.PIPE
+    container_var = (
+        f" PEBBLE_SOCKET=/charm/containers/{container}/pebble.socket"
+        if container
+        else ""
     )
+    cmd = shlex.split(
+        rf"juju ssh {target.unit_name}{container_var} /charm/bin/pebble {command}"
+    )
+    proc = subprocess.Popen(cmd, bufsize=1000, stdout=subprocess.PIPE)
     proc.wait()
     return proc.stdout.read()
 
 
-def _tail_pebble_service_logs(
-    target: Target, pane: ConsolePane, container: str, service_name: str
+def _update_layout_with_service_logs(
+    target: Target, layout: PaneLayout, service_name: str, container: str = None
 ):
-    cmd = _pebble_cmd(target, command=f"logs {service_name} -f", container=container)
-    _redirect_logs(cmd, pane)
+    container_var = (
+        f" PEBBLE_SOCKET=/charm/containers/{container}/pebble.socket"
+        if container
+        else ""
+    )
+    cmd = rf"juju ssh {target.unit_name}{container_var} /charm/bin/pebble logs {service_name}"
+
+    # put pebble logs into a file
+    out = subprocess.getoutput(cmd)
+    layout.pane.clear()
+    layout.pane.add_row(cmd)
+
+    for line in out.splitlines():
+        layout.pane.add_row(line)
 
 
 def get_container_names(target: Target) -> Tuple[str, ...]:
@@ -111,7 +116,54 @@ class _Service:
 
 def get_services(target: Target, container: str) -> Tuple[_Service, ...]:
     out = _pebble(target, container=container, command="services")
-    return tuple(_Service(line.split()[0].strip()) for line in out.splitlines()[1:])
+    return tuple(
+        _Service(line.split()[0].strip().decode("utf-8"))
+        for line in out.splitlines()[1:]
+    )
+
+
+def _pane_name(container: str, service: _Service):
+    return f"{container}::{service.name}"
+
+
+def make_layout(containers_to_services: Dict[str, Iterable[_Service]]) -> Layout:
+    """Define the layout."""
+    layout = Layout(name="root")
+
+    layout.split(
+        Layout(name="header", size=3),
+        Layout(name="main", ratio=1),
+        Layout(name="footer", size=7),
+    )
+
+    layout["main"].split_row(
+        *(Layout(name=container) for container in containers_to_services)
+    )
+
+    for container, services in containers_to_services.items():
+        service_layouts = []
+        for service in services:
+            svc_pane = Pane(border_style="green", expand=True, title=service)
+            service_layouts.append(
+                *(
+                    PaneLayout(svc_pane, name=_pane_name(container, service))
+                    for service in services
+                )
+            )
+        layout[container].split_column(*service_layouts)
+    return layout
+
+
+class Header:
+    def __rich__(self) -> Panel:
+        grid = Pane.grid(expand=True)
+        grid.add_column(justify="center", ratio=1)
+        grid.add_column(justify="right")
+        grid.add_row(
+            "[b]jhack debug-log[/b]",
+            datetime.now().ctime().replace(":", "[blink]:[/]"),
+        )
+        return Panel(grid, style="white on blue")
 
 
 def _tail_logs(
@@ -120,59 +172,64 @@ def _tail_logs(
     workload_logs: bool = True,
 ):
     target = Target.from_name(target)
+    container_to_services = {
+        container: get_services(target, container)
+        for container in get_container_names(target)
+    }
 
-    tails = []
-    layout = Layout()
-    if workload_logs:
-        workload_layouts_inner = Layout(name="workloads")
-        container_layouts = []
-        for container in get_container_names(target):
-            container_layout = Layout(name=container)
-            services_layouts = []
-            for service in get_services(target, container):
-                name = f"{container}:{service.name}"
-                svc_pane = ConsolePane()
-                # services_layouts.append(Panel(svc_pane, title=name))
-                services_layouts.append(svc_pane)
-                tails.append(
-                    [
-                        _tail_pebble_service_logs,
-                        (target, svc_pane, container, service.name),
-                    ]
+    layout = make_layout(container_to_services)
+    layout["header"].update(Header())
+    layout["footer"].update(str(container_to_services))
+
+    def update_all():
+        for container, services in container_to_services.items():
+            for service in services:
+                _update_layout_with_service_logs(
+                    target,
+                    layout[_pane_name(container, service)],
+                    service_name=service.name,
                 )
-            container_layout.split_column(*services_layouts)
-            container_layouts.append(container_layout)
 
-        workload_layouts_inner.split_row(*container_layouts)
-        workload_layout = Layout(Panel(workload_layouts_inner, title="workloads"))
-    else:
-        workload_layout = Layout(visible=False)
-
-    if kubernetes_log:
-        k8s_pane = ConsolePane()
-        k8s_layout = Layout(Panel(k8s_pane, title="k8s"), name="k8s")
-    else:
-        k8s_layout = Layout(visible=False)
-
-    layout.split_row(
-        workload_layout,
-        k8s_layout,
-    )
-
-    tp = ThreadPool()
-
-    with Live(layout):
-        results = []
-        for tail in tails:
-            results.append(tp.apply_async(*tail))
-
+    with Live(layout, auto_refresh=False, screen=True) as live:
         try:
             while True:
-                sleep(1)
+                update_all()
+
+                live.refresh()
+                sleep(0.5)
 
         except KeyboardInterrupt:
-            tp.terminate()
-            exit("interrupted by user")
+            live.stop()
+            exit("interrupted.")
+
+    # if kubernetes_log:
+    #     k8s_pane = Pane()
+    #     k8s_layout = Layout(Panel(k8s_pane, title="k8s"), name="k8s")
+    # else:
+    #     k8s_layout = Layout(visible=False)
+
+    # layout.split_row(
+    #     workload_layout,
+    #     k8s_layout,
+    # )
+    #
+    # try:
+    #     with Live(layout, auto_refresh=False) as live:
+    #         threads = []
+    #         for fn, args in tails:
+    #             thread = Thread(target=fn, args=args)
+    #             threads.append(thread)
+    #             thread.start()
+    #
+    #             while True:
+    #                 sleep(0.2)
+    #                 live.refresh()
+    #
+    # except KeyboardInterrupt:
+    #     for thread in threads:
+    #         thread.join()
+    #     exit("interrupted by user")
+    #
 
 
 def tail_logs(
